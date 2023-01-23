25222e773961ab61efb45e981c95fb3d52378d3d
Maura <113387525+maura16@users.noreply.github.com>
2023-01-23 13:20:34 -0700

    Update install.sh script to include all python versions greater than 3.8

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 function install() {
-    if pip --version 2>&1 | grep -q -e "python3.[6-9]" -e "python 3.[6-9]"; then
+    if pip --version 2>&1 | grep -q -e "python.*3.[8-9]" -e "python.*3.[1][0-1]"; then
         PIP=pip
-    elif pip3 --version 2>&1 | grep -q -e "python3.[6-9]" -e "python 3.[6-9]"; then
+    elif pip3 --version 2>&1 | grep -q -e "python.*3.[8-9]" -e "python.*3.[1][0-1]"; then
         PIP=pip3
     else
-        echo "Minitrino requires Python 3.6+. Please install a compatible Python version and ensure Pip points to it."
+        echo "Minitrino requires Python 3.8+. Please install a compatible Python version and ensure Pip points to it."
         exit 1
     fi
 

a896ea5316fa5c117bddb5751d203e2d1942238c
Jeffrey Lester <jeffrey.d.lester@gmail.com>
2024-03-11 00:13:52 -0700

    Fix curl loop verifying API is accessible

--- src/lib/modules/catalog/redis/resources/bootstrap/bootstrap.sh
+++ src/lib/modules/catalog/redis/resources/bootstrap/bootstrap.sh
@@ -3,12 +3,19 @@
 set -euxo pipefail
 
 # Wait for Redis API to become available
-until $(curl -v -k -s -o /dev/null -w "%{http_code}" https://localhost:9443 | awk '{exit $1>=500?1:0}'); do
-  printf '.'
+set +e
+while true
+do
+  response=$(curl -k -s https://localhost:9443/v1/status)
+  if [[ "$response" != *'{"description":"No cluster bootstrapped","error_code":"no_cluster"}'* ]]; then
+    break
+  fi
   sleep 5
 done
 
 # Create Redis cluster
+set -e
+sleep 5
 curl -v -k -X POST -H "Content-Type: application/json" -d '{
   "action": "create_cluster",
   "cluster": {
@@ -31,12 +38,14 @@ curl -v -k -X POST -H "Content-Type: application/json" -d '{
 }' https://localhost:9443/v1/bootstrap/create_cluster
 
 # Wait for cluster creation to complete
+set +e
 until $(curl -k -u "admin@redis.com:changeit" --output /dev/null --silent --head --fail https://localhost:9443/v1/cluster); do
   printf '.'
   sleep 5
 done
 
 # Join Redis cluster
+set -e
 curl -v -k -X POST -H "Content-Type: application/json" -d '{
   "action": "join_cluster",
   "cluster": {

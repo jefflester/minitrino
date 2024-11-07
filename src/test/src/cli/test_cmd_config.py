254ff64be9c214e006a9dfa5c6c6a38e68ca9c9b
Jeffrey Lester <jeffrey.d.lester@gmail.com>
2024-11-06 18:39:14 -0800

    Add config command tests

--- src/test/src/cli/test_cmd_config.py
+++ src/test/src/cli/test_cmd_config.py
@@ -1,11 +1,6 @@
 #!usr/bin/env/python3
 # -*- coding: utf-8 -*-
 
-# 1. Test with no Minitrino directory
-# 2. Test with no Minitrino config file (ensure template is created)
-# 3. Test reset w/ existing config dir and file (ensure template is created)
-# 4. Test editing an invalid config file
-
 import os
 import subprocess
 
@@ -16,105 +11,89 @@ from inspect import currentframe
 from types import FrameType
 from typing import cast
 
-# Using the Click CliRunner does not appear to play well with commands that
-# require user input. That is being researched.
+CMD = ["-e", "TEXT_EDITOR=cat", "config"]
 
 
 def main():
     common.log_status(__file__)
-    # test_no_directory()
-    test_reset_with_directory()
-    # test_edit_invalid_config()
-    # test_edit_valid_config()
+    test_no_directory()
+    test_no_config_file()
+    test_reset()
+    test_edit_invalid_config()
+    test_edit_valid_config()
 
 
 def test_no_directory():
-    """Verifies that a configuration directory and config file are created with
-    config --reset."""
+    """Verifies that a configuration directory and config file are created when
+    executing config command (if not already present)."""
 
     common.log_status(cast(FrameType, currentframe()).f_code.co_name)
 
     subprocess.call(f"rm -rf {common.MINITRINO_USER_DIR}", shell=True)
-    return_code = subprocess.call("minitrino config", shell=True)
+    result = helpers.execute_command(CMD)
 
-    assert return_code == 0
+    assert result.exit_code == 0
     assert os.path.isdir(common.MINITRINO_USER_DIR)
     assert os.path.isfile(common.CONFIG_FILE)
 
     common.log_success(cast(FrameType, currentframe()).f_code.co_name)
 
 
-def test_reset_with_directory():
-    """Verifies that the configuration directory is only removed and restored
-    with the user's approval. This is a valid test case for both 'yes' and 'no'
-    responses."""
-
-    import time
+def test_no_config_file():
+    """Verifies that a missing config file is created when executing config
+    command."""
 
     common.log_status(cast(FrameType, currentframe()).f_code.co_name)
 
-    subprocess.call(
-        f"mkdir {common.MINITRINO_USER_DIR}", shell=True, stdout=subprocess.DEVNULL
-    )
+    subprocess.call(f"rm {common.CONFIG_FILE}", shell=True)
+    result = helpers.execute_command(CMD)
 
-    start_time = time.time()
-    end_time = 2.0
-    output = ""
-    while time.time() - start_time <= end_time:
-        process = subprocess.Popen(
-            "minitrino -v config --reset",
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            universal_newlines=True,
-        )
-        while True:
-            output_line = process.stdout.readline()
-            if output_line == "":
-                break
-        output, _ = process.communicate()  # Get full output (stdout + stderr)
-        if time.time() >= end_time:
-            process.terminate()
-            break
+    assert result.exit_code == 0
+    assert os.path.isfile(common.CONFIG_FILE)
 
-    process = subprocess.Popen(
-        "minitrino -v config --reset",
-        stdout=subprocess.PIPE,
-        stdin=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
+    common.log_success(cast(FrameType, currentframe()).f_code.co_name)
+
+
+def test_reset():
+    """Ensures that the --reset option works as expected, even if the config
+    file is invalid."""
+
+    common.log_status(cast(FrameType, currentframe()).f_code.co_name)
+
+    subprocess.call(f"echo 'hello world' > {common.CONFIG_FILE}", shell=True)
+
+    # Run the command and feed 'y' as input
+    result = helpers.execute_command(CMD + ["--reset"], command_input="y\n")
+
+    assert result.exit_code == 0
+    assert os.path.isfile(common.CONFIG_FILE)
+
+    config_contents = subprocess.Popen(
+        f"cat {common.CONFIG_FILE}",
         shell=True,
+        stdout=subprocess.PIPE,
         universal_newlines=True,
     )
-    output = process.communicate(input="y\n", timeout=1)[0]
-    process.terminate()
 
-    assert process.returncode == 0
-    assert all(
-        (
-            "Configuration directory exists" in output,
-            "Created Minitrino configuration directory" in output,
-            "Opening existing config file at path" in output,
-        )
-    )
-    assert os.path.isdir(common.MINITRINO_USER_DIR)
-    assert os.path.isfile(common.CONFIG_FILE)
+    config_contents, _ = config_contents.communicate()
+    assert "hello world" not in config_contents
 
     common.log_success(cast(FrameType, currentframe()).f_code.co_name)
 
 
 def test_edit_invalid_config():
     """Verifies that an error is not thrown if the config is 'invalid' (such as
-    a missing section or value required to perform an action). This is because
-    there is default behavior built in, and all major functions should still
-    work without a valid configuration file."""
+    a missing section). This is because there is default behavior built in, and
+    all major functions should still work without a valid configuration file."""
 
     common.log_status(cast(FrameType, currentframe()).f_code.co_name)
 
-    subprocess.call(f"rm -rf {common.MINITRINO_USER_DIR}", shell=True)
-    subprocess.call(f"mkdir {common.MINITRINO_USER_DIR}", shell=True)
-    subprocess.call(f"touch {common.CONFIG_FILE}", shell=True)
-    return_code = subprocess.call(f"minitrino config", shell=True)
-    assert return_code == 0
+    subprocess.call(f"echo 'hello world' > {common.CONFIG_FILE}", shell=True)
+    result = helpers.execute_command(CMD)
+
+    assert result.exit_code == 0
+    assert "Failed to parse config file" in result.output
+
     common.log_success(cast(FrameType, currentframe()).f_code.co_name)
 
 
@@ -123,11 +102,10 @@ def test_edit_valid_config():
 
     common.log_status(cast(FrameType, currentframe()).f_code.co_name)
 
-    subprocess.call(f"rm -rf {common.MINITRINO_USER_DIR}", shell=True)
-    subprocess.call(f"mkdir {common.MINITRINO_USER_DIR}", shell=True)
     helpers.make_sample_config()
-    return_code = subprocess.call(f"minitrino config", shell=True)
-    assert return_code == 0
+    result = helpers.execute_command(CMD)
+    assert result.exit_code == 0
+
     common.log_success(cast(FrameType, currentframe()).f_code.co_name)
 
 

390f1a9d8d31e1b8d16602a8879ac7ccd6a83b2b
Jeffrey Lester <jeffrey.d.lester@gmail.com>
2025-04-12 00:33:11 -0700

    Split Dockerfile into stages to cache first stage; update install script

--- src/lib/image/src/scripts/install.sh
+++ src/lib/image/src/scripts/install.sh
@@ -9,34 +9,43 @@ GID="${4}"
 
 echo "Installing ${CLUSTER_DIST}-${CLUSTER_VER} for user ${USER} (UID=${UID}, GID=${GID})..."
 
+normalize_arch() {
+    local raw_arch
+    raw_arch="$(uname -m)"
+
+    case "${raw_arch}" in
+        amd64|x86_64)
+            ARCH_SEP_S3="x86_64"
+            ARCH_BIN="amd64"
+            ;;
+        arm64|aarch64)
+            ARCH_SEP_S3="aarch64"
+            ARCH_BIN="arm64"
+            ;;
+        *)
+            echo "Unsupported architecture: ${raw_arch}"
+            exit 1
+            ;;
+    esac
+}
+
 set_dist_version() {
     if [ "${CLUSTER_DIST}" == "trino" ]; then
         TRINO_VER="${CLUSTER_VER}"
     elif [ "${CLUSTER_DIST}" == "starburst" ]; then
         TRINO_VER="${CLUSTER_VER:0:3}"
         STARBURST_VER="${CLUSTER_VER}" # For clarity
+        # Handle Starburst archive naming changes at 462+
+        if [ "${TRINO_VER}" -ge 462 ]; then
+            STARBURST_VER_ARCH="${STARBURST_VER}${ARCH_SEP_S3:+.$ARCH_SEP_S3}"
+            STARBURST_VER_ARCH_UNPACK="${STARBURST_VER}${ARCH_SEP_S3:+-$ARCH_SEP_S3}"
+        else
+            STARBURST_VER_ARCH="${STARBURST_VER}"
+            STARBURST_VER_ARCH_UNPACK="${STARBURST_VER}"
+        fi
     else
         echo "Invalid cluster distribution. Exiting..."
         exit 1
-    fi
-}
-
-check_arch() {
-    if [ "${TRINO_VER}" -ge 462 ]; then
-        case "$(uname -m)" in
-            x86_64|amd64)
-                ARCH="x86_64"
-                ;;
-            aarch64|arm64)
-                ARCH="aarch64"
-                ;;
-            *)
-                echo "Unsupported architecture: $(uname -m)"
-                exit 1
-                ;;
-        esac
-    else
-        ARCH=""
     fi
 }
 
@@ -51,29 +60,42 @@ create_app_user() {
 
 create_directories() {
     echo "Creating application directories..."
-    mkdir -p /usr/lib/"${CLUSTER_DIST}"/ /data/"${CLUSTER_DIST}"/ /home/"${CLUSTER_DIST}"/
+    mkdir -p \
+        /usr/lib/"${CLUSTER_DIST}"/ \
+        /data/"${CLUSTER_DIST}"/ \
+        /home/"${CLUSTER_DIST}"/
+}
+
+prune() {
+    echo "Pruning unnecessary application files..."
+    cd /tmp/
+    if [ "${CLUSTER_DIST}" == "trino" ]; then
+        cp -R "trino-server-${TRINO_VER}/"* /usr/lib/"${CLUSTER_DIST}"/
+        rm -rf /usr/lib/"${CLUSTER_DIST}"/bin/darwin-* /usr/lib/"${CLUSTER_DIST}"/bin/linux-*
+        cp -R "trino-server-${TRINO_VER}/bin/linux-${ARCH_BIN}" /usr/lib/"${CLUSTER_DIST}"/bin/
+    else
+        cp -R "starburst-enterprise-${STARBURST_VER_ARCH_UNPACK}"/* /usr/lib/"${CLUSTER_DIST}"/
+        rm -rf /usr/lib/"${CLUSTER_DIST}"/bin/darwin-* /usr/lib/"${CLUSTER_DIST}"/bin/linux-*
+        cp -R \
+            "starburst-enterprise-${STARBURST_VER_ARCH_UNPACK}/bin/linux-${ARCH_BIN}" \
+            /usr/lib/"${CLUSTER_DIST}"/bin/
+    fi
 }
 
 download_and_extract() {
     echo "Downloading application tarball..."
+    cd /tmp/
     if [ "${CLUSTER_DIST}" == "trino" ]; then
-        cd /tmp/
         TAR_FILE="trino-server-${TRINO_VER}.tar.gz"
-        curl -fsSL -o "${TAR_FILE}" \
-            https://repo1.maven.org/maven2/io/trino/trino-server/"${TRINO_VER}"/trino-server-"${TRINO_VER}".tar.gz
-        tar xvfz "${TAR_FILE}"
-        cp -R "trino-server-${TRINO_VER}"/* /usr/lib/"${CLUSTER_DIST}"/
-    else # Download Starburst
-        STARBURST_VER_ARCH="${STARBURST_VER}${ARCH:+.$ARCH}"
-        STARBURST_VER_ARCH_UNPACK="${STARBURST_VER}${ARCH:+-$ARCH}"
-        BUCKET="s3.us-east-2.amazonaws.com/software.starburstdata.net"
+        curl -#LfS -o "${TAR_FILE}" \
+            "https://repo1.maven.org/maven2/io/trino/trino-server/${TRINO_VER}/trino-server-${TRINO_VER}.tar.gz"
+    else
         TAR_FILE="starburst-enterprise-${STARBURST_VER_ARCH}.tar.gz"
-        cd /tmp/
-        curl -fsSL -o "${TAR_FILE}" \
-            "${BUCKET}/${STARBURST_VER:0:3}/${STARBURST_VER}/${TAR_FILE}"
-        tar xvfz "${TAR_FILE}"
-        cp -R "starburst-enterprise-${STARBURST_VER_ARCH_UNPACK}"/* /usr/lib/"${CLUSTER_DIST}"/
+        curl -#LfS -o "${TAR_FILE}" \
+            "https://s3.us-east-2.amazonaws.com/software.starburstdata.net/${STARBURST_VER:0:3}e/${STARBURST_VER}/${TAR_FILE}"
     fi
+    tar xvfz "${TAR_FILE}"
+    prune
 }
 
 copy_scripts() {
@@ -81,7 +103,7 @@ copy_scripts() {
     cp /tmp/run-minitrino.sh /usr/lib/"${CLUSTER_DIST}"/bin/
 }
 
-set_ownership_and_permissions() {
+set_ownership_and_perms() {
     echo "Setting directory ownership and permissions..."
     chown -R "${USER}":"${GROUP}" \
         /usr/lib/"${CLUSTER_DIST}" \
@@ -98,7 +120,7 @@ set_ownership_and_permissions() {
 configure_jvm() {
     echo "Configuring jvm.config..."
     cd /etc/"${CLUSTER_DIST}"/
-    curl -fsSL -o jvm.config \
+    curl -#LfS -o jvm.config \
         https://raw.githubusercontent.com/trinodb/trino/"${TRINO_VER}"/core/docker/default/etc/jvm.config
     chmod g+w jvm.config
     chown "${USER}":"${GROUP}" jvm.config
@@ -107,7 +129,7 @@ configure_jvm() {
     echo "-Djavax.net.ssl.trustStorePassword=changeit" >> jvm.config
 }
 
-configure_node_properties() {
+configure_node_props() {
     echo "Configuring node.properties..."
     sed -i \
     -e "s|^node\.data-dir=.*|node.data-dir=/data/${CLUSTER_DIST}|" \
@@ -123,7 +145,7 @@ install_java() {
 install_trino_cli() {
     echo "Installing trino-cli..."
     TRINO_CLI_PATH="/usr/local/bin/trino-cli"
-    curl -fsSL -o "${TRINO_CLI_PATH}" \
+    curl -#LfS -o "${TRINO_CLI_PATH}" \
         "https://repo1.maven.org/maven2/io/trino/trino-cli/${TRINO_VER}/trino-cli-${TRINO_VER}-executable.jar"
     chmod +x "${TRINO_CLI_PATH}"
     chown "${USER}":"${GROUP}" "${TRINO_CLI_PATH}"
@@ -145,15 +167,15 @@ cleanup() {
 }
 
 main() {
+    normalize_arch
     set_dist_version
-    check_arch
     create_app_user
     create_directories
     download_and_extract
     copy_scripts
-    set_ownership_and_permissions
+    set_ownership_and_perms
     configure_jvm
-    configure_node_properties
+    configure_node_props
     install_java
     install_trino_cli
     install_wait_for_it

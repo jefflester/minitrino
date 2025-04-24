9cc0b6cc49ceeed2c18f12c3e2820814b588cd82
Jeffrey Lester <jeffrey.d.lester@gmail.com>
2025-04-24 09:22:36 -0700

    Remove pid file if it exists as part of entrypoint

--- src/lib/image/src/scripts/run-minitrino.sh
+++ src/lib/image/src/scripts/run-minitrino.sh
@@ -4,6 +4,9 @@ set -euxo pipefail
 
 ulimit -c 4096
 
+# Remove PID file if it exists
+rm -f /data/${CLUSTER_DIST}/var/run/launcher.pid
+
 launcher_opts=(--etc-dir /etc/${CLUSTER_DIST})
 if ! grep -s -q 'node.id' /etc/${CLUSTER_DIST}/node.properties; then
     launcher_opts+=("-Dnode.id=${HOSTNAME}")

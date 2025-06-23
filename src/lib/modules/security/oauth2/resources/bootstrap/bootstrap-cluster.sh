8202eb6c26a12cc48267f3cd691e10772306b244
Jeffrey Lester <jeffrey.d.lester@gmail.com>
2025-06-22 23:39:14 -0700

    Update oauth module; add users and include group claims

--- src/lib/modules/security/oauth2/resources/bootstrap/bootstrap-cluster.sh
+++ src/lib/modules/security/oauth2/resources/bootstrap/bootstrap-cluster.sh
@@ -9,11 +9,28 @@ before_start() {
         > /tmp/oauth2-server.pem
 
     echo "Adding OAuth2 server certificate to truststore..."
-    keytool -import -noprompt -trustcacerts \
+    if ! keytool -list \
+        -keystore /etc/starburst/tls-jvm/cacerts \
         -storepass changeit \
-        -alias oauth2-server \
-        -file /tmp/oauth2-server.pem \
-        -keystore /etc/"${CLUSTER_DIST}"/tls-jvm/cacerts
+        -alias oauth2-server > /dev/null 2>&1
+    then
+        keytool -import -noprompt -trustcacerts \
+            -storepass changeit \
+            -alias oauth2-server \
+            -file /tmp/oauth2-server.pem \
+            -keystore /etc/"${CLUSTER_DIST}"/tls-jvm/cacerts
+    else
+        echo "Alias 'oauth2-server' already exists in truststore. Skipping import."
+    fi
+
+    oauth_debug="io.trino.server.security.oauth2=DEBUG"
+    ui_debug="io.trino.server.ui.OAuth2WebUiAuthenticationFilter=DEBUG"
+    if ! grep -q "${oauth_debug}" /etc/"${CLUSTER_DIST}"/log.properties; then
+        echo "${oauth_debug}" >> /etc/"${CLUSTER_DIST}"/log.properties
+    fi
+    if ! grep -q "${ui_debug}" /etc/"${CLUSTER_DIST}"/log.properties; then
+        echo "${ui_debug}" >> /etc/"${CLUSTER_DIST}"/log.properties
+    fi
 }
 
 after_start() {

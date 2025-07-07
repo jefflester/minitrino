d0914987fece2adf9446bfedbdcb0db7b3913659
Jeffrey Lester <jeffrey.d.lester@gmail.com>
2025-07-07 12:02:21 -0700

    Patch BIAC bootstrap to create _stub role for programmatic CLI access to admin-style role

--- src/lib/modules/security/biac/resources/cluster/bootstrap.sh
+++ src/lib/modules/security/biac/resources/cluster/bootstrap.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+## Filename prefixed with 00 to ensure this runs first
+
 set -euxo pipefail
 
 before_start() {
@@ -21,5 +23,58 @@ before_start() {
 }
 
 after_start() {
-    :
+    # Create _stub role so that the admin user auto-uses an admin-like role when
+    # connecting via the trino-cli
+    echo "Creating stub role..."
+    role_id=$(curl -X POST \
+        -H "X-Trino-User: admin" \
+        -H "X-Trino-Role: system=ROLE{sysadmin}" \
+        -H "Accept: application/json" \
+        -H "Content-Type: application/json" \
+        -d '{
+            "name": "_stub",
+            "description": "Admin role used for cluster bootstrapping."
+        }' \
+        "http://localhost:8080/api/v1/biac/roles" | jq .id || true
+    )
+
+    if [ -z "${role_id}" ] || [ "${role_id}" = "null" ]; then
+    echo "Stub role not created, attempting to look up existing stub role..."
+    role_id=$(curl -s -X GET \
+        -H "X-Trino-User: admin" \
+        -H "X-Trino-Role: system=ROLE{sysadmin}" \
+        -H "Accept: application/json" \
+        "http://localhost:8080/api/v1/biac/roles" | jq -r '.result[] | select(.name=="_stub") | .id')
+    fi
+
+    echo "Assigning admin role to stub role..."
+    curl -X POST \
+    -H "X-Trino-User: admin" \
+    -H "X-Trino-Role: system=ROLE{sysadmin}" \
+    -H "Accept: application/json" \
+    -H "Content-Type: application/json" \
+    -d "{
+        \"roleId\": \"${role_id}\",
+        \"roleAdmin\": true
+    }" \
+    "http://localhost:8080/api/v1/biac/subjects/users/admin/assignments" || true
+
+    echo "Granting privs to stub role..."
+    actions=(SHOW CREATE ALTER DROP EXECUTE SELECT INSERT DELETE UPDATE)
+    for action in "${actions[@]}"; do
+    curl -X POST \
+        -H "X-Trino-User: admin" \
+        -H "X-Trino-Role: system=ROLE{sysadmin}" \
+        -H "Accept: application/json" \
+        -H "Content-Type: application/json" \
+        -d "{
+        \"effect\": \"ALLOW_WITH_GRANT_OPTION\",
+        \"action\": \"${action}\",
+        \"entity\": {
+            \"category\": \"TABLES\",
+            \"allEntities\": true
+        }
+        }" \
+        "http://localhost:8080/api/v1/biac/roles/${role_id}/grants" || true
+    done
 }
